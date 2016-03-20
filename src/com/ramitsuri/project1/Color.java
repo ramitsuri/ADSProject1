5430e071e7d3ce10acf247fd363ba7dc06cd7172
ramitsuri <ramitsuri007@gmail.com>
2016-03-19 23:32:27 -0400

    removed Color and RBNode

--- src/com/ramitsuri/project1/Color.java
+++
@@ -1,9 +0,0 @@
-package com.ramitsuri.project1;
-
-/**
- * Created by ramitsuri on 3/13/16.
- */
-public class Color {
-    public static boolean RED = false;
-    public static boolean BLACK = true;
-}
